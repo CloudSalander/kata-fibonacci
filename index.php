7d0c5f39547db063f643318bcece8e33ac06e601
Cloud <ruben.as532@gmail.com>
2023-05-18 10:03:19 +0200

    First Fibonacci version(with arrays)

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+function get_fibonacci_serie(int $n): array {
+	$fibonacci_serie = array();
+	$fibonacci_serie[0] = 0;
+	if($n > 0) $fibonacci_serie[1] = 1;
+	if($n > 1) {
+		$con = 2;
+		while($con <= $n) {
+			$fibonacci_serie[$con] = $fibonacci_serie[$con-2] + $fibonacci_serie[$con-1];
+			++$con;
+		}
+	}
+	return $fibonacci_serie; 
+}
+
+function printFibonacciSerie(array $serie) {
+	var_dump($serie);
+}
+
+
+$n = intval(readline("What Fibonacci series do you want to know?"));
+$serie = get_fibonacci_serie($n);
+printFibonacciSerie($serie);
+
+?>
